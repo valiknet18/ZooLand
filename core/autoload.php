02924c0da50b243c5939b63b219ca6888b50ac47
Valik <valik.v1per@gmail.com>
2014-10-23 00:20:02 +0300

    Config for use twig without composer

--- core/autoload.php
+++ core/autoload.php
@@ -1,5 +1,5 @@
 <?php
-require_once "../vendor/autoload.php";
+require_once "../vendor/twig/twig/lib/Twig/Autoloader.php";
 require_once "libs/URL.php";
 require_once "route.php";
 
--- core/libs/Twig.php
+++ core/libs/Twig.php
@@ -4,6 +4,7 @@ abstract class Twig {
     private $twig;
 
     private  function init(){
+        Twig_Autoloader::register();
         $loader = new Twig_Loader_Filesystem('../web/view');
         $this->twig = new Twig_Environment($loader);
     }
